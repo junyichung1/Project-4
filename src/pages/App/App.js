e83896be7ef18cdcd449b7abbcc79294eb53a781
Junyi Chung <junyichung@junyis-air.home>
2020-10-10 13:36:02 -0700

    create navbar

---
+++ src/pages/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import NavBar from '../../components/NavBar/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <NavBar />
+      </header>
+    </div>
+  );
+}
+
+export default App;
