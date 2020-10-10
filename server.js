98bb0a88eea9972103dff0b0e821792ba41fbf0a
Junyi Chung <junyichung@junyis-air.home>
2020-10-10 14:36:06 -0700

    authentification

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
 
+const app = express();
+
 require('dotenv').config();
 require('./config/database');
-
-const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -17,6 +17,8 @@ app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
 // Put API routes here, before the "catch all" route
+app.use('/api/users', require('./routes/api/users'));
+app.use(require('./config/auth'));
 
 // The following "catch all" route (note the *)is necessary
 // for a SPA's client-side routing to properly work
